b5d02831b1e6d29d04dee02d78cc76e3a188485f
Justin Reeves <reeves122@gmail.com>
2022-05-31 15:09:54 -0500

    use latest lambda module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,8 @@ locals {
   }
 }
 
-data "aws_region" "this" {}
-data "aws_caller_identity" "this" {}
-
 module "lambda" {
-  depends_on                      = [module.ecr]
-  source                          = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.19-1702466"
+  source                          = "github.com/champ-oss/terraform-aws-lambda.git?ref=f2dab2956d11c427cdf6ab54d791655997e4beb6"
   git                             = var.git
   name                            = "lambda"
   vpc_id                          = var.enable_vpc ? var.vpc_id : null
@@ -20,19 +16,12 @@ module "lambda" {
   enable_function_url             = true
   function_url_authorization_type = "NONE"
   reserved_concurrent_executions  = var.reserved_concurrent_executions
-  ecr_account                     = data.aws_caller_identity.this.account_id
-  ecr_name                        = aws_ecr_repository.this.name
+  sync_image                      = true
+  sync_source_repo                = "champtitles/iris"
+  ecr_name                        = "${var.git}-lambda"
   ecr_tag                         = var.docker_tag
   tags                            = merge(local.tags, var.tags)
   environment = {
     ALLOWED_URLS = join(",", var.allowed_urls)
   }
-}
-
-module "ecr" {
-  source           = "github.com/champ-oss/terraform-aws-ecr.git?ref=v1.0.22-24fb4c0"
-  name             = "${var.git}-lambda"
-  sync_image       = true
-  sync_source_repo = "champtitles/iris"
-  sync_source_tag  = var.docker_tag
 }
