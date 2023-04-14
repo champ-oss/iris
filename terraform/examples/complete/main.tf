446423d6b78897eb79c14d4e6088daba9c42c8be
Justin Reeves <reeves122@gmail.com>
2023-04-14 09:47:49 -0500

    add backend block and tags

--- terraform/examples/complete/main.tf
+++ terraform/examples/complete/main.tf
@@ -1,15 +1,24 @@
+terraform {
+  backend "s3" {}
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 locals {
   git = "iris"
+  tags = {
+    cost    = "shared"
+    creator = "terraform"
+    git     = local.git
+  }
 }
 
 module "this" {
   source     = "../../"
   enable_vpc = false
-
+  tags       = local.tags
   allowed_urls = [
     "about.google/google-in-america",
     "aws.amazon.com/console",
